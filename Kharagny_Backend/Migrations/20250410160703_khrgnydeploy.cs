ab5bf5032f5970edabd4d3ba6282b0109739e5e8
Salma Tomas <2021170813@cis.asu.edu.eg>
2025-04-10 18:12:36 +0200

    deploy

---
+++ Kharagny_Backend/Migrations/20250410160703_khrgnydeploy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kharagny_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class khrgnydeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
